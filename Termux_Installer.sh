658064dc41c44ad35e97b6a680d386056ab8f931
Haroon Awan <mrharoonawan@Gmail.com>
2020-01-03 10:02:55 -0500

    Update Termux_Installer.sh

--- Termux_Installer.sh
+++ Termux_Installer.sh
@@ -22,11 +22,11 @@
 
 clear
 
-echo "					XSS Fuzz Swiss Knife on any URI     Version 1.0a";   
-echo "						Termux Installer By: Haroon Awan";
-echo "							Coded By: Haroon Awan";
-echo "						   Mail: mrharoonawan@gmail.com";
-echo "						      Debug:Hackeruniversee";
+echo "[-] XSS Scanner and Finder, ver 1.0a";   
+echo "[+] Termux Installer By: Haroon Awan";
+echo "[+] Coded By: Haroon Awan";
+echo "[+] Mail: mrharoonawan@gmail.com";
+echo "[+] Debug:Hackeruniversee";
 echo "";
 
 
@@ -37,7 +37,7 @@ echo -e "Installing Modules"
 pip2 install -r requirements.txt
 pip2 install httplib2
 pip2 install mechanize
-apt-get instgall gem
+pkg install ruby
 gem install watir
 
 echo -e "[+] Installed Success!";
